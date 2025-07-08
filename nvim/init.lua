65248e8bb6c590f51e5198eb8d3b2a0123332cbc
phondani0 <ankitphondani52@gmail.com>
2025-07-08 23:25:01 +0530

    fix: update deprecated plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
--- Lazy.nvim bootstrap (plugin manager setup)
+-- Plugin manager setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -8,11 +8,11 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Load all plugins
+-- Plugins
 require("lazy").setup({
-  { "neovim/nvim-lspconfig" },
-  { "williamboman/mason.nvim" },
-  { "williamboman/mason-lspconfig.nvim" },
+  { "neovim/nvim-lspconfig" },            -- LSP client
+  { "williamboman/mason.nvim" },          -- LSP installer
+  { "williamboman/mason-lspconfig.nvim" },-- LSP config helper
 
   { "hrsh7th/nvim-cmp" },
   { "hrsh7th/cmp-nvim-lsp" },
@@ -22,14 +22,17 @@ require("lazy").setup({
   { "saadparwaiz1/cmp_luasnip" },
   { "rafamadriz/friendly-snippets" },
 
-  { "nvim-tree/nvim-tree.lua" }, -- file sidebar
+  { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
   { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
   { "lewis6991/gitsigns.nvim" },
-  { "nvimtools/none-ls.nvim" },
+
+  { "stevearc/conform.nvim" },            -- Formatter
+  { "mfussenegger/nvim-lint" },           -- Diagnostics
+
   { "folke/tokyonight.nvim" },
 })
 
--- General editor settings
+-- Basic settings
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 2
@@ -42,32 +45,29 @@ vim.opt.cursorline = true
 vim.opt.signcolumn = "yes"
 vim.g.mapleader = " "
 
--- Theme setup
 vim.cmd.colorscheme("tokyonight-night")
 
--- LSP setup
+-- LSP installer + setup
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "tsserver", "eslint", "html", "cssls", "jsonls" }
+  ensure_installed = { "ts_ls", "eslint", "html", "cssls", "jsonls" }
 })
 
 local lspconfig = require("lspconfig")
-lspconfig.tsserver.setup({})
+lspconfig.ts_ls.setup({})
 lspconfig.eslint.setup({})
 lspconfig.html.setup({})
 lspconfig.cssls.setup({})
 lspconfig.jsonls.setup({})
 
--- Autocomplete (cmp) config
+-- Autocomplete configuration
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
   snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
+    expand = function(args) luasnip.lsp_expand(args.body) end,
   },
   mapping = cmp.mapping.preset.insert({
     ["<C-Space>"] = cmp.mapping.complete(),
@@ -81,53 +81,41 @@ cmp.setup({
   },
 })
 
--- File sidebar (nvim-tree) setup
+-- File explorer sidebar
 require("nvim-tree").setup({
-  view = {
-    width = 30,
-    side = "left",
-  },
-  renderer = {
-    icons = {
-      show = {
-        git = false,
-        folder = false,
-        file = false,
-        folder_arrow = false,
-      }
-    }
-  },
-  git = {
-    enable = false,
-  }
+  view = { width = 30, side = "left" },
 })
-
--- Toggle file sidebar with Ctrl+n
 vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
--- Telescope shortcuts
-vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>", {})
-vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", {})
+-- Fuzzy search
+vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>")
+vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>")
 
--- Git signs config (without icons or signs)
-require("gitsigns").setup({
-  signs = false,
-  numhl = true,
-})
+-- Git signs in gutter
+require("gitsigns").setup()
 
--- Formatter and linter setup (using null-ls)
-local null_ls = require("null-ls")
-null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.prettier,
-    null_ls.builtins.diagnostics.eslint,
+-- Formatter setup via conform
+require("conform").setup({
+  formatters_by_ft = {
+    javascript = { "prettierd" },
+    typescript = { "prettierd" },
+    ["*"] = { "trim_whitespace" },
   },
 })
+vim.keymap.set("n", "<leader>f", require("conform").format, { desc = "Format file" })
 
--- Format on save for TS/JS files
-vim.cmd [[autocmd BufWritePre *.ts,*.tsx,*.js,*.jsx lua vim.lsp.buf.format()]]
+-- Diagnostics setup via nvim-lint
+require("lint").linters_by_ft = {
+  javascript = { "eslint_d" },
+  typescript = { "eslint_d" },
+}
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  callback = function()
+    require("lint").try_lint()
+  end,
+})
 
--- LSP key mappings
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+-- Keybindings for LSP actions
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
